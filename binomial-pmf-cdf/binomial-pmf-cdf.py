120caace0c9286891f1ccae0d47aa4ba0f6eef94
Nikunj Bhardwaj <nikunjbhardwaj3030@gmail.com>
2026-03-18 12:41:53 +0530

    Update solution for binomial-pmf-cdf

--- binomial-pmf-cdf/binomial-pmf-cdf.py
+++ binomial-pmf-cdf/binomial-pmf-cdf.py
@@ -5,10 +5,12 @@ def binomial_pmf_cdf(n, p, k):
     """
     Compute Binomial PMF and CDF.
     """
-    # PMF: P(X = k) = comb(n, k) * (p**k) * ((1-p)**(n-k))
+    # Calculate Probability Mass Function (PMF) for X = k
+    # P(X = k) = comb(n, k) * (p**k) * ((1-p)**(n-k))
     pmf = float(comb(n, k) * (p**k) * ((1 - p)**(n - k)))
     
-    # CDF: P(X <= k) = sum of PMF from 0 to k
+    # Calculate Cumulative Distribution Function (CDF) for X <= k
+    # We sum the PMF values for all i from 0 to k
     cdf = 0.0
     for i in range(k + 1):
         cdf += comb(n, i) * (p**i) * ((1 - p)**(n - i))
